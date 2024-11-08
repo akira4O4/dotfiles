e7e1a207be7329cdeecfd4a639b61ddca97e0827
Akira.404 <akira.404@foxmail.com>
2024-11-08 14:43:58 +0800

    update

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 echo "Link tmux.conf"
-sudo ln -s -f ~/tmux-installer/tmux.conf ~/.tmux.conf
+sudo ln -s -f ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 echo "Link tmux.conf.local"
-sudo ln -s -f ~/tmux-installer/tmux.conf.local ~/.tmux.conf.local
+sudo ln -s -f ~/dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
+
+echo "Link condarc"
+sudo ln -s -f ~/dotfiles/conda/condarc ~/.condarc
+
+echo "Link zshrc"
+sudo ln -s -f ~/dotfiles/zsh/zshrc ~/.zshrc
 
 # Append aliases to .bashrc
-echo '# Tmux' >> ~/.bashrc
-echo 'alias tns="tmux new -s"' >> ~/.bashrc
-echo 'alias tl="tmux ls"' >> ~/.bashrc
-echo 'alias ta="tmux attach -t"' >> ~/.bashrc
-echo 'alias tk="tmux kill-session -t"' >> ~/.bashrc
+echo '# Tmux' >> ~/.zshrc
+echo 'alias tns="tmux new -s"' >> ~/.zshrc
+echo 'alias tl="tmux ls"' >> ~/.zshrc
+echo 'alias ta="tmux attach -t"' >> ~/.zshrc
+echo 'alias tk="tmux kill-session -t"' >> ~/.zshrc
 echo '# Tmux' >> ~/.bashrc
 
 # Notify the user to reload .bashrc
-echo "Aliases added to .bashrc. Please run 'source ~/.bashrc' to apply changes."
+#echo "Aliases added to .bashrc. Please run 'source ~/.bashrc' to apply changes."
